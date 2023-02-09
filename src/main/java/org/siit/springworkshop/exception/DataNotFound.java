65b9f6b806d353652e9e8e5bca3330af9f202655
Emi14 <emiionut@gmail.com>
2023-02-09 21:36:14 +0200

    Updated postman collection + DataNotFound exception

--- src/main/java/org/siit/springworkshop/exception/DataNotFound.java
+++ src/main/java/org/siit/springworkshop/exception/DataNotFound.java
@@ -1,6 +1,6 @@
 package org.siit.springworkshop.exception;
 
-public class DataNotFound extends Exception{
+public class DataNotFound extends Exception {
 
     public DataNotFound(String message) {
         super(message);
